824a139980c2bb3444f20c12955699f3f5c18037
nim <100094899+nimblemindedone@users.noreply.github.com>
2023-08-03 20:48:09 +0100

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,1 +1,15 @@
-{"3262475518", "4868543686", "546280191"}
+while wait() do
+function plrFromChr(character)for _, player in pairs(game:GetService("Players"):GetPlayers())do if player.Character==character then return(player)end end end
+	for i,v in pairs(workspace:GetDescendants())do
+		if not plrFromChr(v)and v.Name=='Mesh'and not v:IsDescendantOf(workspace.SecureParts) then
+			v:Destroy()
+        end
+    end
+    for i,v in game.workspace:GetDescendants() do
+        if v.ClassName == 'Humanoid' then
+            v.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
+            v.HealthDisplayDistance = 0
+            v.NameDisplayDistance = 0
+        end
+    end
+end
