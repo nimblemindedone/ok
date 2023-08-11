3441404962a5b036f0680add0d208de4a41661c6
nim <100094899+nimblemindedone@users.noreply.github.com>
2023-08-11 15:13:01 +0100

    Update noname.lua

--- noname.lua
+++ noname.lua
@@ -1,3 +1,12 @@
+for i,v in game.workspace:GetDescendants() do
+	if v.ClassName == 'Humanoid' then
+    	if v.Name ~= 'Cleaned' then
+            v.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
+            v.NameDisplayDistance = 0
+            v.Name = 'Cleaned'
+        end
+    end
+end
 while wait(5) do
 for i,v in game.workspace:GetDescendants() do
 	if v.ClassName == 'Humanoid' then
