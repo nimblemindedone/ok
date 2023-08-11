4d1878cd37b0ccaf16d591f5a07557986f9ccf91
nim <100094899+nimblemindedone@users.noreply.github.com>
2023-08-11 15:17:31 +0100

    Update noname.lua

--- noname.lua
+++ noname.lua
@@ -1,21 +1,19 @@
-repeat wait() until game:IsLoaded()
-for i,v in game.workspace:GetDescendants() do
-	if v.ClassName == 'Humanoid' then
-    	if v.Name ~= 'Cleaned' then
-            v.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
-            v.NameDisplayDistance = 0
-            v.Name = 'Cleaned'
+while wait() do
+    for i,v in pairs(workspace:GetDescendants())do
+        if v.Name=='Mesh' or v.Name=='SpecialMesh' then
+            if v.Parent.Name~='Head' and v.Parent.Name~='Handle' then
+                if not v:IsDescendantOf(Workspace.SecureParts) then
+                    v:Destroy()
+                end
+            end
         end
     end
-end
-while wait(5) do
-for i,v in game.workspace:GetDescendants() do
-	if v.ClassName == 'Humanoid' then
-    	if v.Name ~= 'Cleaned' then
-            v.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
-            v.NameDisplayDistance = 0
-            v.Name = 'Cleaned'
+    for i,v in game.workspace:GetDescendants() do
+        if v.ClassName == 'Humanoid' then
+            if v.Name ~= 'Cleaned' then
+                v.NameDisplayDistance = 0
+                v.Name = 'Cleaned'
+            end
         end
     end
-end
 end
