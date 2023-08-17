57255da9c9e97fa91f1efb3ee3484ced2ab3c085
nim <100094899+nimblemindedone@users.noreply.github.com>
2023-08-17 17:55:25 +0100

    Update noname.lua

--- noname.lua
+++ noname.lua
@@ -2,6 +2,13 @@ local Players = game:GetService("Players")
 local Player = Players.LocalPlayer or Players.LocalPlayerAdded:wait()
 local Daddy = Players:FindFirstChild('0lxad') or Players:WaitForChild('0lxad')
 local runservice = game:GetService("RunService")
+local function onDescendantAdded(descendant)
+	if descendant.ClassName == 'Humanoid' then
+    descendant.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
+    descendant.NameDisplayDistance = 0
+    descendant.Name = 'Fixed by nim'
+  end
+end
 
 rchat = function(Message)
   Players:Chat(Message)
@@ -31,13 +38,5 @@ runservice.Stepped:Connect(function()
             end
         end
     end
-    for i,v in game.workspace:GetDescendants() do
-        if v.ClassName == 'Humanoid' then
-            if v.Name ~= 'Cleaned' then
-                v.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.Subject
-                v.NameDisplayDistance = 0
-                v.Name = 'Cleaned'
-            end
-        end
-    end
 end)
+workspace.DescendantAdded:Connect(onDescendantAdded)
