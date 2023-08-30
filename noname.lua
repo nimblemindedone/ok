86aa278cb4e6118246f927de7cb4e466b9f7ed70
nim <100094899+nimblemindedone@users.noreply.github.com>
2023-08-31 00:10:45 +0100

    Update noname.lua

--- noname.lua
+++ noname.lua
@@ -133,3 +133,143 @@ say("Loaded Omega's anti lag")
 wait(2)
 camera.CameraSubject = Player.Character
 game.Chat.BubbleChatEnabled = false
+local httpservice = game:GetService('HttpService')
+
+_G.noice = false
+_G.nojail = false
+_G.nofling = false
+_G.nopunish = false
+rchat = function(Message)
+  Players:Chat(Message)
+end
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local GUI = Mercury:Create{
+    Name = "Omega",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://roblox.com/Admin-House"
+}
+local Tab = GUI:Tab{
+	Name = "Anti's",
+	Icon = "rbxassetid://8569322835"
+}
+Tab:Toggle{
+	Name = "Anti-Freeze/Ice (requires admin)",
+	StartingState = false,
+	Description = "Made by Cizz",
+	Callback = function(state)
+		_G.noice = state
+		if state then
+    	if Player.Character.HumanoidRootPart.Anchored then
+    		rchat(':thaw')
+    	end
+		end
+		GUI:Notification{
+			Title = "Omega's Utils Remastered",
+			Text = ("Anti-Freeze", state),
+			Duration = 8,
+			Callback = function() end
+		}
+	end
+}
+
+Tab:Toggle{
+	Name = "Anti-Jail (requires admin)",
+	StartingState = false,
+	Description = "Made by Cizz",
+	Callback = function(state)
+		_G.nojail = state
+		if state then
+		if workspace:FindFirstChild(Player.Name.."_ADONISJAIL") then
+    	rchat(':free')
+		end
+		end
+		GUI:Notification{
+			Title = "Omega's Utils Remastered",
+			Text = ("Anti-Jail", state),
+			Duration = 8,
+			Callback = function() end
+		}
+	end
+}
+
+Tab:Toggle{
+	Name = "Anti-Punish (requires admin)",
+	StartingState = false,
+	Description = "Made by Cizz",
+	Callback = function(state)
+		_G.nopunish = state
+		if state then
+		if Player.Character.Parent == game.Lighting or Player.Character.Parent.ClassName == 'Model' then
+			rchat(':unpunish') 
+    	end
+		end
+		GUI:Notification{
+			Title = "Omega's Utils Remastered",
+			Text = ("Anti-Punish", state),
+			Duration = 8,
+			Callback = function() end
+		}
+	end
+}
+
+local Tab = GUI:Tab{
+	Name = "Random",
+	Icon = "rbxassetid://8569322835"
+}
+
+GUI:Credit{
+	Name = "Cizz/Omega",
+	Description = "Made the script",
+	V3rm = nil,
+	Discord = "afvk/0megaa."
+}
+
+GUI:Credit{
+	Name = "Lucid",
+	Description = "Is my guy",
+	V3rm = nil,
+	Discord = "1ucd"
+}
+
+GUI:Credit{
+	Name = "Iconic",
+	Description = "Cool",
+	V3rm = nil,
+	Discord = "just_iconic"
+}
+
+Player.CharacterAdded:Connect(function(char)
+	char:GetPropertyChangedSignal('Parent'):Connect(function()
+	if _G.nopunish then
+		task.wait()
+		if char.Parent == game.Lighting or char.Parent.ClassName == 'Model' then
+			task.wait()
+			rchat(':unpunish') 
+    	end
+	end
+	end)
+	char:WaitForChild("HumanoidRootPart"):GetPropertyChangedSignal('Anchored'):Connect(function()
+	if _G.noice then
+		if char.HumanoidRootPart.Anchored then
+			task.wait()
+			rchat(":thaw")
+		end
+	end
+	end)
+end)
+
+workspace.DescendantAdded:Connect(function(descendant)
+	if _G.nojail then
+	if descendant.Name == (Player.Name..'_ADONISJAIL') then
+		task.wait()
+		rchat(":Free")
+	end
+	end
+end)
+GUI:Notification{
+	Title = "Omega's Utils Remastered",
+	Text = "Welcome to OU:R (Alpha)",
+	Duration = 8,
+	Callback = function() end
+}
